ccb753d323ab6d6ef194ecaab5dffcc712364095
Andrey Grabarnick <andreyg@exypnos.co.il>
2019-06-17 16:23:27 +0300

    Add Rotation Function

--- tangram-ga/gui.py
+++ tangram-ga/gui.py
@@ -19,21 +19,40 @@ class CreateCanvas(Frame):
 
         canvas.create_line(rectangle, dash=(4, 2), width=5)
 
-        shape = [(0, 0), (100, 0), (100, 100), (0, 100), (0, 0)]
+        shape = [(0, 0), (100, 0), (100, 100), (0, 0)]
 
-        canvas.create_polygon(get_shape(shape, 100, 100),
+        canvas.create_polygon(get_shape(shape, 100, 100, 90),
                               outline="#fff", fill='#f00')
 
         canvas.pack(fill=BOTH, expand=1)
 
 
-def get_shape(points, X, Y):
+def rotate(points, angle, center):
+    angle = math.radians(angle)
+    cos_val = math.cos(angle)
+    sin_val = math.sin(angle)
+    cx, cy = center
+    new_points = []
+    for x_old, y_old in points:
+        x_old -= cx
+        y_old -= cy
+        x_new = x_old * cos_val - y_old * sin_val
+        y_new = x_old * sin_val + y_old * cos_val
+        new_points.append([x_new + cx, y_new + cy])
+    return new_points
+
+
+def get_shape(points, X, Y, angle):
     coords = []
-    print(points)
+    centroid = ((sum([p[0] for p in points]) / len(points))+X,
+                (sum([p[1] for p in points]) / len(points))+Y)
+
     for i in range(len(points)):
-        coords.append(points[i][0]+X)
-        coords.append(points[i][1]+Y)
-    return coords
+        coords.append((points[i][0]+X, points[i][1]+Y))
+    print(rotate(coords, angle, centroid))
+    # rotate
+
+    return rotate(coords, angle, centroid)
 
 
 def main():
