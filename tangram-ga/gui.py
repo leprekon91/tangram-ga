0c876e1076b753fde2a0d0903b401804f85a91b0
Andrey Grabarnick <andreyg@exypnos.co.il>
2019-07-08 15:34:39 +0300

    Add function to draw a single genome and clean up code

--- tangram-ga/gui.py
+++ tangram-ga/gui.py
@@ -1,19 +1,6 @@
 from tkinter import Tk, X, Label, Canvas, Frame, BOTH
-from tangram import TangramShape
+from tangram import TangramShape, shapeArray
 import math
-
-bgTri_1 = TangramShape([(0, 0), (320, 320), (0, 640), (0, 0)], '#f00', 0)
-bgTri_2 = TangramShape([(0, 0), (320, 320), (0, 640), (0, 0)], '#0f0', 90)
-mdTri = TangramShape([(0, 0), (320, 0), (0, 320), (0, 0)], '#00f', 180)
-smTri_1 = TangramShape([(0, 0), (320, 0), (160, 160), (0, 0)], '#ff0', 180)
-smTri_2 = TangramShape([(0, 0), (320, 0), (160, 160), (0, 0)], '#f0f', 90)
-
-sqr = TangramShape([(160, 160), (320, 320), (160, 480),
-                    (0, 320), (160, 160)], '#0ff', 0)
-
-paral = TangramShape([(0, 160), (160, 0), (480, 0),
-                      (320, 160), (0, 160)], '#faf', 0)
-
 
 canvas_padding = 20
 
@@ -31,31 +18,42 @@ class CreateCanvas(Frame):
         self.pack(fill=BOTH, expand=1)
 
         canvas = Canvas(self)
-        rectangle = [canvas_padding, canvas_padding, 640+canvas_padding,
-                     canvas_padding, canvas_padding + 640, canvas_padding + 640, canvas_padding, canvas_padding + 640, canvas_padding, canvas_padding]
+        rectangle = [0, 0, 640, 0, 640, 640, 0, 640, 0, 0]
+        for i in range(len(rectangle)):
+            rectangle[i] += canvas_padding
 
         canvas.create_line(rectangle, dash=(4, 2), width=5)
 
-        # draw all shapes
-        canvasDraw(canvas, bgTri_1, 0, 0)
-        canvasDraw(canvas, bgTri_2, 320, -160)
-        canvasDraw(canvas, mdTri, 480, 480)
-        canvasDraw(canvas, smTri_1, 240, 400)
-        canvasDraw(canvas, smTri_2, 480, 80)
-        canvasDraw(canvas, sqr, 320, 0)
-        canvasDraw(canvas, paral, 0, 480)
+        # draw an example genome
+        example_genome = [
+            [0, 0, 0],
+            [320, -160, 90],
+            [480, 480, 180],
+            [240, 400, 180],
+            [480, 80, 90],
+            [320, 0, 0],
+            [0, 480, 0],
+        ]
 
+        drawGenome(canvas, example_genome)
         canvas.pack(fill=BOTH, expand=1)
 
 
-def canvasDraw(canvas, shape, positionX, positionY):
-    points = shape.getRotatedPoints(
-        positionX+canvas_padding, positionY+canvas_padding)
-    canvas.create_polygon(points, outline="#000", fill=shape.color)
+def drawGenome(canvas, genome):
+    for i in range(len(genome)):
+        shapeArray[i].angle = genome[i][2]
+        points = shapeArray[i].getRotatedPoints(
+            genome[i][0]+canvas_padding,
+            genome[i][1]+canvas_padding
+        )
+        canvas.create_polygon(
+            points,
+            outline="#000",
+            fill=shapeArray[i].color
+        )
 
 
 def main():
-
     root = Tk()
     ex = CreateCanvas()
     root.geometry("700x700+100+100")
--- tangram-ga/tangram.py
+++ tangram-ga/tangram.py
@@ -1,4 +1,5 @@
 import math
+
 # shape object
 
 
@@ -7,6 +8,12 @@ class TangramShape:
         self.points = points
         self.color = color
         self.angle = angle
+        self.posX = 0
+        self.posY = 0
+
+    def setPosition(self, x, y):
+        self.posX = x
+        self.posY = y
 
     def getCenter(self, X, Y):
         centroid = ((sum([p[0] for p in self.points]) / len(self.points))+X,
@@ -32,3 +39,17 @@ class TangramShape:
             new_points.append([x_new + cx, y_new + cy])
         return new_points
 
+# possible shapes
+bgTri_1 = TangramShape([(0, 0), (320, 320), (0, 640), (0, 0)], '#f00', 0)
+bgTri_2 = TangramShape([(0, 0), (320, 320), (0, 640), (0, 0)], '#0f0', 90)
+mdTri = TangramShape([(0, 0), (320, 0), (0, 320), (0, 0)], '#00f', 180)
+smTri_1 = TangramShape([(0, 0), (320, 0), (160, 160), (0, 0)], '#ff0', 180)
+smTri_2 = TangramShape([(0, 0), (320, 0), (160, 160), (0, 0)], '#f0f', 90)
+
+sqr = TangramShape([(160, 160), (320, 320), (160, 480),
+                    (0, 320), (160, 160)], '#0ff', 0)
+
+paral = TangramShape([(0, 160), (160, 0), (480, 0),
+                      (320, 160), (0, 160)], '#555', 0)
+
+shapeArray = [bgTri_1,bgTri_2,mdTri,smTri_1,smTri_2,sqr,paral]
